6be3211df3ec6199af18cf7dbe0c92aac6525de7
Narsima <narsimachilkuri123@outlook.com>
2022-11-08 21:18:03 +0530

    added gif for establishing connection

--- bigbluebutton-html5/imports/ui/components/audio/audio-modal/component.jsx
+++ bigbluebutton-html5/imports/ui/components/audio/audio-modal/component.jsx
@@ -471,6 +471,15 @@ class AudioModal extends Component {
   }
 
   renderContent() {
+
+    const audioTitle = {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      fontSize: '20px',
+      textAlign: 'center'
+    }
+
     const {
       isEchoTest,
       intl,
@@ -482,10 +491,10 @@ class AudioModal extends Component {
     if (this.skipAudioOptions()) {
       return (
         <Styled.Connecting role="alert">
-          <span data-test={!isEchoTest ? 'establishingAudioLabel' : 'connectingToEchoTest'}>
-            {intl.formatMessage(intlMessages.connecting)}
-          </span>
-          <Styled.ConnectingAnimation animations={animations} />
+          <span data-test={!isEchoTest ? 'establishingAudioLabel' : 'connectingToEchoTest'} style={audioTitle}>
+            <img class="avia_image" src="https://api.dev.aieze.in/api/v1/admin/cdn/file?file_name=public/audio-loader-light-green.gif" alt="How to build a CDN Server" title="CDN.request-flow" itemprop="thumbnailUrl" style={audioConnectingGif} />
+            {intl.formatMessage(intlMessages.connecting)} </span>
+          {/* <Styled.ConnectingAnimation animations={animations} /> */}
         </Styled.Connecting>
       );
     }
