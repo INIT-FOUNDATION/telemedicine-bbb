da5c32c531dd91c90852d38247d0c45c906b55ef
Narsima <narsimachilkuri123@outlook.com>
2022-11-12 11:46:17 +0530

    position chat button at bottom

--- bigbluebutton-html5/imports/ui/components/user-list/chat-list-item/styles.js
+++ bigbluebutton-html5/imports/ui/components/user-list/chat-list-item/styles.js
@@ -75,6 +75,8 @@ const ChatListItem = styled(Styled.ListItem)`
   margin-left: ${borderSize};
   margin-top: ${borderSize};
   margin-bottom: ${borderSize};
+  position: absolute;
+  bottom: 0;
   margin-right: 0;
 
   [dir="rtl"] & {
